f520a23a592db91df04609cd13a4e3fcd9390331
igor morais <igoracmorais@gmail.com>
2021-08-09 23:41:10 -0300

    Add files via upload

---
+++ inserindo_LR_no_scorecard/woebin_20210616_230040.py
@@ -0,0 +1,10 @@
+breaks_list={
+'Age': [23.0,30.0,32.0,40.0,44.0,48.0,54.0], 
+'Checking account': ['rich','moderate','little'], 
+'Credit amount': [800.0,2400.0,3800.0,4800.0,6800.0,8800.0], 
+'Duration': [10.0,14.0,18.0,22.0,30.0,44.0], 
+'Housing': ['free','own','rent'], 
+'Job': [2.0,3.0], 
+'Purpose': ['domestic appliances%,%vacation/others%,%repairs%,%car','radio/TV','furniture/equipment','business%,%education'], 
+'Saving accounts': ['little','quite rich','rich%,%moderate'], 
+'Sex': ['female','male']}
